2396aa4eccb17ef86d36b8224e543585d43fede3
Yifan Song <yson0084@student.monash.edu>
2025-09-28 01:23:07 +0800

    fix the bug

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,5 @@
 // vega_lite_vis.js
-const specUrl = "specs/mys_crude_exports_2023_map.vg.json";
+const specUrl = "specs/mys_crude_exports_2023_map.vg.json?v=1"; // v 参数防缓存
 vegaEmbed("#export_map", specUrl, {
   actions: { export: true, source: true, editor: true, compiled: false }
 }).catch(console.error);
