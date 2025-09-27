3de932e3763d877fbcbcd100f2c047bff18def1b
Yifan Song <yson0084@student.monash.edu>
2025-09-28 00:50:38 +0800

    fix the error

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,7 +1,5 @@
 // vega_lite_vis.js
-var vg_1 = "specs/mys_crude_exports_2023_map.vg.json";
-vegaEmbed("#export_map", vg_1)   // ← 这里改成 #export_map
-  .then(function (result) {
-    // result.view 可做进阶控制
-  })
-  .catch(console.error);
+const specUrl = "specs/mys_crude_exports_2023_map.vg.json";
+vegaEmbed("#export_map", specUrl, {
+  actions: { export: true, source: true, editor: true, compiled: false }
+}).catch(console.error);
